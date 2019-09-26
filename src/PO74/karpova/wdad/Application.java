2f3ec12696411e5bdb88a1ad19bffa52dc3a6ca9
KDO-PO <sudeki21@gmail.com>
2019-09-26 17:03:04 +0400

    Added entry point

---
+++ src/PO74/karpova/wdad/Application.java
@@ -0,0 +1,13 @@
+package PO74.karpova.wdad;
+
+import java.io.Console;
+
+public class Application {
+
+
+    public static void main(String[] args){
+
+        System.out.println("I’m Dariy Karpova, and I’m not a monkey");
+}
+}
+
